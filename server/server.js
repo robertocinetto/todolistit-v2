df3b76b2b1af3aa1351719b433f047b900bfb375
Roberto Cinetto <roberto.cinetto@gmail.com>
2022-07-06 18:24:56 -0700

    fix

--- server/server.js
+++ server/server.js
@@ -8,13 +8,12 @@ const PORT = process.env.PORT || 5001
 
 const path = require('path')
 
-app.use(express.static(path.resolve(__dirname, '../client/build')))
-
 // import routes file and set express to use it
 const routes = require('./routes/index')
 app.use('/', routes)
 
-const publicPath = path.join(__dirname, '..', 'client/build')
+const publicPath = path.resolve(__dirname, '../client/public')
+app.use(express.static(publicPath))
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'))
 })
