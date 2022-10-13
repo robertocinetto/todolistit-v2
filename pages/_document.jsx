75a32fc35bc97857484ba63a9d143ee898ec5b3e
Roberto Cinetto <roberto.cinetto@gmail.com>
2022-10-13 13:02:26 -0700

    Fix: disable ios zoom on input focus

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,19 +5,23 @@ const Document = () => {
     <Html>
       <Head>
         <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
+          rel='preconnect'
+          href='https://fonts.googleapis.com'
         />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Exo+2:wght@300;400;600&display=swap"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css2?family=Exo+2:wght@300;400;600&display=swap'
+          rel='stylesheet'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, maximum-scale=1'
         />
       </Head>
-      <body className="bg-white dark:bg-slate-800 dark:text-slate-100 dark:dark-theme">
+      <body className='bg-white dark:bg-slate-800 dark:text-slate-100 dark:dark-theme'>
         <Main />
         <NextScript />
       </body>
