507036e2882e1426b649f51c9463b5fe68c2b11c
Roberto Cinetto <roberto.cinetto@gmail.com>
2022-10-13 20:19:23 -0700

    Feat: category deletion

--- components/CategorySelection.jsx
+++ components/CategorySelection.jsx
@@ -3,7 +3,18 @@ import { useContext, useEffect, useState } from 'react'
 import { useToastContext } from '../hooks/useToastContext'
 import { UserContext } from '../contexts/UserContext'
 
-import { addDoc, collection, onSnapshot, query, serverTimestamp, where } from 'firebase/firestore'
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  onSnapshot,
+  query,
+  serverTimestamp,
+  where,
+  writeBatch,
+} from 'firebase/firestore'
 import { db } from '../firebase'
 
 import { RadioButton } from 'primereact/radiobutton'
@@ -11,18 +22,15 @@ import { Button } from 'primereact/button'
 import { Dialog } from 'primereact/dialog'
 import { InputText } from 'primereact/inputtext'
 import { Dropdown } from 'primereact/dropdown'
+import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog'
 
 import { useRecoilState } from 'recoil'
 import { categoriesState } from '../atom/categoriesAtom'
 
 const CategorySelection = ({ selectedCategory, handleCategoryChange }) => {
   const [displayPopup, setDisplayPopup] = useState(false)
-  const [category, setCategory] = useState(null)
   const [newCategory, setNewCategory] = useState('')
   const [categories, setCategories] = useRecoilState(categoriesState)
-  const [dropdownSelectedCategory, setDropdownSelectedCategory] = useState(null)
-
-  // const [selectedCategory, setSelectedCategory] = useState(categories)
   const { showSuccess } = useToastContext()
   const { user, setUser } = useContext(UserContext)
 
@@ -78,8 +86,42 @@ const CategorySelection = ({ selectedCategory, handleCategoryChange }) => {
     setDisplayPopup(false)
   }
 
+  const deleteCategory = async categoryId => {
+    const batch = writeBatch(db)
+    const q = query(collection(db, 'todos'), where('categoryId', '==', categoryId))
+    const todosToBeDeleted = await getDocs(q)
+    try {
+      todosToBeDeleted.forEach(todo => {
+        console.log(todo.data())
+        batch.delete(todo.ref)
+      })
+      await batch
+        .commit()
+        .then(
+          console.log(categoryId),
+          await deleteDoc(doc(db, 'categories', categoryId)).then(
+            showSuccess(undefined, undefined, 'Category deleted successfully')
+          )
+        )
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const confirmDeleteCategory = categoryId => {
+    confirmDialog({
+      message: 'Are you sure you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      position: 'bottom-right',
+      accept: () => deleteCategory(categoryId),
+      // reject
+    })
+  }
+
   return (
     <div className='flex flex-col justify-between '>
+      {/* <ConfirmDialog /> */}
       <Dropdown
         value={selectedCategory}
         options={categories}
@@ -93,10 +135,10 @@ const CategorySelection = ({ selectedCategory, handleCategoryChange }) => {
           return (
             <div
               key={category.id}
-              className='field-radiobutton flex items-center '
+              className='field-radiobutton flex items-center relative'
             >
               <label
-                className={`cursor-pointer hidden md:block w-full p-2 mb-2 border dark:border-zinc-600 rounded transition-colors ${
+                className={` cursor-pointer hidden md:block w-full p-2 mb-2 border dark:border-zinc-600 rounded transition-colors ${
                   selectedCategory.id === category.id
                     ? 'bg-violet-500 text-white  border-l-8 border-violet-700 dark:border-violet-700'
                     : ''
@@ -113,6 +155,12 @@ const CategorySelection = ({ selectedCategory, handleCategoryChange }) => {
                 />
                 {category.categoryName}
               </label>
+              <Button
+                icon='pi pi-times'
+                className='p-button-rounded p-button-text p-button-plain p-button-xs absolute right-1 top-1.5'
+                aria-label='Cancel'
+                onClick={() => confirmDeleteCategory(category.id)}
+              />
             </div>
           )
         })}
